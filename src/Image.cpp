97e7e3adb7bdea8bc67fc8c2bc9494802a19df76
Markus Glau <57915992+glaumarkus@users.noreply.github.com>
2020-10-05 13:52:03 +0200

    Implementation of opencv

--- src/Image.cpp
+++ src/Image.cpp
@@ -1,10 +1,10 @@
 #include "Image.h"
 
-Image::Image(int width, int height) : 
+Image::Image(int width, int height) :
 	width(width),
 	height(height)
 {
-	imageData.resize(width * height);
+	image = cv::Mat::zeros(height, width, CV_32FC3);
 }
 
 int Image::getHeight() {
@@ -15,24 +15,27 @@ int Image::getWidth() {
 	return width;
 }
 
-void Image::saveImage() {
-
-	std::ofstream out("out.ppm");
-	out << "P3\n" << width << ' ' << height << ' ' << "255\n";
-	std::vector<Color>::iterator it;
-
-	for (
-		it = imageData.begin();
-		it != imageData.end();
-		++it
-		)
-	{
-		out << 
-			(int)it->r << ' ' << 
-			(int)it->g << ' ' <<
-			(int)it->b << ' ' << '\n';
-
-	}
+inline float clamp_quick(const float& f) {
+	return std::max(0.0f, std::min(255.0f, f));
 }
 
-Image::~Image(){}
+void Image::changePixel(const Color& c, const int x, const int y) {
+	float red = clamp_quick(c.r * 255);
+	float blue = clamp_quick(c.b * 255);
+	float green = clamp_quick(c.g * 255);
+	image.at<cv::Vec3f>(x, y) = cv::Vec3f(blue, green, red);
+}
+
+
+bool Image::saveImage(const std::string& filename) {
+
+	// apply blur to soften edges
+	cv::Mat blur;
+	cv::GaussianBlur(image, blur, cv::Size(5, 5), 0);
+
+	if (cv::imwrite(filename, blur))
+		return true;
+	return false;
+}
+
+Image::~Image() {}
