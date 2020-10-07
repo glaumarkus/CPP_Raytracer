35d1e70b30c8c444386dc9541d9eba134c2500dc
Markus Glau <57915992+glaumarkus@users.noreply.github.com>
2020-10-07 23:13:10 +0200

    Update shape.cpp

--- src/shape.cpp
+++ src/shape.cpp
@@ -1,7 +1,44 @@
 #include "shape.h"
 
 
-Shape::~Shape(){}
+void Light::intersect(Intersection& i) {
+
+	Ray localRay = i.ray;
+	localRay.origin -= position;
+
+	float a, b, c;
+	a = localRay.direction.length2();
+	b = 2 * dot(localRay.direction, localRay.origin);
+	c = localRay.origin.length2() - squared(1.0f);
+
+	float discriminant = squared(b) - 4 * a * c;
+
+	float t1 = (-b - std::sqrt(discriminant)) / (2 * a);
+	float t2 = (-b + std::sqrt(discriminant)) / (2 * a);
+
+	if (t1 > RAY_T_MIN && t1 < i.t)
+		i.t = t1;
+	else if (t2 > RAY_T_MIN && t2 < i.t)
+		i.t = t2;
+}
+
+bool Sphere::isPlane() {
+	return false;
+}
+
+bool ShapeSet::isPlane() {
+	return false;
+}
+
+bool Plane::isPlane() {
+	return true;
+}
+
+bool Checkboard::isPlane() {
+	return true;
+}
+
+Shape::~Shape() {}
 
 ShapeSet::ShapeSet()
 {}
@@ -13,21 +50,34 @@ void ShapeSet::addShape(Shape* shape) {
 	shapes.emplace_back(shape);
 }
 
-Color ShapeSet::getColor(const Intersection& i,  Shape* s, LightSet* l) {
+Color ShapeSet::getColor(const Intersection& i, Shape* s, LightSet* l) {
 	return Color();
 }
 
 
-bool Sphere::isShade(Intersection& i, Shape* s, Light* l) {
+bool Sphere::isShade(Intersection& i, Shape* s, Light* l, const float ref_t) {
 
 	bool isShade = false;
 	if (s->intersect(i)) {
-		isShade = true;
+		if (i.t < ref_t) {
+			isShade = true;
+		}
 	}
 	return isShade;
 }
 
-Color Sphere::getColor(const Intersection& i,  Shape* s, LightSet* l) {
+bool Plane::isShade(Intersection& i, Shape* s, Light* l, const float ref_t) {
+
+	bool isShade = false;
+	if (s->intersect(i)) {
+		if ( i.t < ref_t ) {
+			isShade = true;
+		}
+	}
+	return isShade;
+}
+
+Color Sphere::getColor(const Intersection& i, Shape* s, LightSet* l) {
 
 	Color finColor;
 	const Point pt = i.ray.calculate(i.t);
@@ -35,7 +85,7 @@ Color Sphere::getColor(const Intersection& i,  Shape* s, LightSet* l) {
 	std::list<Light*>::iterator it;
 
 	const int numLights = l->lights.size();
-	
+
 	for (it = l->lights.begin(); it != l->lights.end(); ++it) {
 
 		Light* curLight = *it;
@@ -51,10 +101,14 @@ Color Sphere::getColor(const Intersection& i,  Shape* s, LightSet* l) {
 		Vector3 normal = getNormalFromPt(pt);
 		float lDotN = dot(lightVector, normal);
 
+		Ray lightRay(pt, lightVector);
+		Intersection lightTest(lightRay);
+		curLight->intersect(lightTest);
+
 		Ray shadeRay(pt, lightVector);
 		Intersection shadeTest(shadeRay);
 
-		if (!isShade(shadeTest, s, curLight)) {
+		if (!isShade(shadeTest, s, curLight, lightTest.t)) {
 
 			if (lDotN >= 0) {
 
@@ -74,12 +128,100 @@ Color Sphere::getColor(const Intersection& i,  Shape* s, LightSet* l) {
 	return finColor;
 }
 
-Color Plane::getColor(const Intersection& i,  Shape* s, LightSet* l) {
-	return mat.color;
+
+Color Plane::getColor(const Intersection& i, Shape* s, LightSet* l) {
+
+
+
+	Color finColor;
+	const Point pt = i.ray.calculate(i.t);
+	const Vector3 pov = i.ray.direction;
+
+	Color reflectColor = Color();
+	Color ambientFinal= Color();
+	Color diffuseFinal = Color();
+	Color specularFinal = Color();
+
+	std::list<Light*>::iterator it;
+
+	const int numLights = l->lights.size();
+
+	for (it = l->lights.begin(); it != l->lights.end(); ++it) {
+
+		Light* curLight = *it;
+
+		Point lightCenter(curLight->position);
+		Vector3 lightVector = (lightCenter - pt).normalized();
+
+		Color effectiveColor = mat.color * curLight->intensity;
+		Color ambientColor = effectiveColor * mat.ambient;
+		Color diffuseColor = Color();
+		Color specularColor = Color();
+
+		float lDotN = dot(lightVector, normal);
+
+		Ray lightRay(pt, lightVector);
+		Intersection lightTest(lightRay);
+		curLight->intersect(lightTest);
+
+		Ray shadeRay(pt, lightVector);
+		Intersection shadeTest(shadeRay);
+
+		if (!isShade(shadeTest, s, curLight, lightTest.t)) {
+
+			if (lDotN >= 0) {
+
+				diffuseColor = effectiveColor * mat.diffuse * lDotN;
+				Vector3 reflection = reflect((lightVector * -1), normal);
+				float rDotE = dot(reflection, pov);
+
+				if (rDotE < 0) {
+					float factor = std::pow(rDotE, mat.shinyness);
+					specularColor = curLight->color * mat.specular * factor;
+				}
+			}
+		}
+
+		//ambientFinal += ambientColor;
+		//diffuseFinal += diffuseColor;
+		//specularFinal += specularColor;
+
+		finColor += (ambientColor + diffuseColor + specularColor) * mat.color;
+	}
+
+	//ambientFinal = ambientFinal * (1.f / numLights);
+	//diffuseFinal = diffuseFinal * (1.f / numLights);
+	//specularFinal = specularFinal * (1.f / numLights);
+
+	finColor = finColor * (1.f / numLights);
+	
+
+	if (mat.refraction > 0.1) {
+		Vector3 reflectVec = reflect(pov, normal);
+		Ray reflectRay(pt, reflectVec);
+		Intersection reflectI(reflectRay);
+		if (s->intersect(reflectI)) {
+
+			//reflectColor
+
+			/*
+			if (&i.pShape == &this) {
+				reflectColor = i.pShape->getColor(i, s, l);
+			}
+			*/
+			
+		}
+		finColor = finColor * (1.f - mat.refraction) + reflectColor * mat.refraction;
+	}
+
+	
+	return finColor;
 }
 
-Color Checkboard::getColor(const Intersection& i,  Shape* s, LightSet* l) {
-	
+
+
+Color Checkboard::getColor(const Intersection& i, Shape* s, LightSet* l) {
+
 	Point p = i.ray.calculate(i.t);
 	return getBaseColor(p);
 }
@@ -106,7 +248,7 @@ bool ShapeSet::intersect(Intersection& i) {
 	std::list<Shape*>::iterator it;
 	for (it = shapes.begin(); it != shapes.end(); it++) {
 
-		Shape *curShape = *it;
+		Shape* curShape = *it;
 		if (curShape->intersect(i)) {
 			doesIntersect = true;
 		}
@@ -114,13 +256,13 @@ bool ShapeSet::intersect(Intersection& i) {
 	return doesIntersect;
 }
 
-Plane::Plane(const Point& position, const Vector3& normal, const Material& mat) : 
+Plane::Plane(const Point& position, const Vector3& normal, const Material& mat) :
 	position(position),
 	normal(normal),
 	mat(mat)
 {}
 
-Checkboard::Checkboard(const Point& position, const Vector3& normal, const Material& mat, float tilesize) : 
+Checkboard::Checkboard(const Point& position, const Vector3& normal, const Material& mat, float tilesize) :
 	position(position),
 	normal(normal),
 	mat(mat),
@@ -163,7 +305,7 @@ bool Checkboard::intersect(Intersection& i) {
 	return true;
 }
 
-Plane::~Plane(){}
+Plane::~Plane() {}
 Checkboard::~Checkboard() {}
 
 Sphere::Sphere(const Point& center, const float radius, const Material& mat) :
@@ -213,7 +355,7 @@ Vector3 Sphere::getNormalFromPt(const Point& p) {
 Sphere::~Sphere()
 {}
 
-Light::Light(const Point& position, const Color& color, const float intensity) : 
+Light::Light(const Point& position, const Color& color, const float intensity) :
 	position(position),
 	color(color),
 	intensity(intensity)
@@ -222,9 +364,9 @@ Light::Light(const Point& position, const Color& color, const float intensity) :
 Light::~Light()
 {}
 
-LightSet::LightSet(){}
+LightSet::LightSet() {}
 
-LightSet::~LightSet(){}
+LightSet::~LightSet() {}
 
 void LightSet::addLight(Light* light) {
 	lights.emplace_back(light);
